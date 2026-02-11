b1d1274c3af29971671802df8815b16910829fd6
M-Hanzala <hhanzala419@gmail.com>
2026-02-11 17:14:52 +0500

    Dashboard In Construction.

--- dashboard/app.js
+++ dashboard/app.js
@@ -1,5 +1,17 @@
-// document.querySelectorAll(".header-mid i").forEach(icon => {
-//     icon.addEventListener("click", () => {
-//         icon.style.color = "#0866FF";
-//     })
-// })
+const icons = document.querySelectorAll(".header-mid i");
+const homeIcon = document.getElementById("home");
+
+icons.forEach(icon => {
+    homeIcon.style.color = "#0866FF";
+
+    icon.addEventListener("click", () => {
+
+        // reset all icons to grey
+        icons.forEach(i => {
+            i.style.color = "#65676B";
+        });
+
+        // make clicked icon blue
+        icon.style.color = "#0866FF";
+    });
+});
