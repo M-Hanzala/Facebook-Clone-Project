859070624005dcd4935b3826205308911d2c8592
M-Hanzala <hhanzala419@gmail.com>
2026-02-14 16:59:27 +0500

    Stories Generated By Map

--- dashboard/app.js
+++ dashboard/app.js
@@ -16,117 +16,139 @@ icons.forEach(icon => {
     });
 });
 
+// // Left side See more & less
+// const seeMore = document.getElementById("seeMore");
+// const seeLess = document.getElementById("seeLess");
+
+// const seeMoreElements = document.querySelectorAll("#seeMoreElement");
+
+// seeMore.addEventListener("click", () => {
+//     seeMoreElements.forEach(el => {
+//         el.classList.add("show");
+//     });
+//     seeLess.style.display = "block";
+// })
+
+// seeLess.addEventListener("click", () => {
+//     seeMoreElements.forEach(el => {
+//         el.classList.remove("show");
+//     });
+//     seeLess.style.display = "none";
+// })
+
 // Stories
 let storyContainer = document.getElementById("story-container");
 
 const stories = [
     {
         fullName: "Emma Johnson",
-        profilePic: "https://randomuser.me/api/portraits/women/44.jpg",
-        storyFile: "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee"
+        profilePic: "https://randomuser.me/api/portraits/women/11.jpg",
+        storyFile: "https://images.unsplash.com/photo-1506744038136-46273834b3fb?w=400&q=60"
     },
     {
         fullName: "Liam Carter",
-        profilePic: "https://randomuser.me/api/portraits/men/32.jpg",
-        storyFile: "https://images.unsplash.com/photo-1494790108377-be9c29b29330"
+        profilePic: "https://randomuser.me/api/portraits/men/12.jpg",
+        storyFile: "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?w=400&q=60"
     },
-    // {
-    //     fullName: "Sophia Martinez",
-    //     profilePic: "https://randomuser.me/api/portraits/women/65.jpg",
-    //     storyFile: "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d"
-    // },
-    // {
-    //     fullName: "Noah Williams",
-    //     profilePic: "https://randomuser.me/api/portraits/men/45.jpg",
-    //     storyFile: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e"
-    // },
-    // {
-    //     fullName: "Olivia Brown",
-    //     profilePic: "https://randomuser.me/api/portraits/women/21.jpg",
-    //     storyFile: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e"
-    // },
-    // {
-    //     fullName: "James Anderson",
-    //     profilePic: "https://randomuser.me/api/portraits/men/18.jpg",
-    //     storyFile: "https://images.unsplash.com/photo-1517841905240-472988babdf9"
-    // },
-    // {
-    //     fullName: "Ava Taylor",
-    //     profilePic: "https://randomuser.me/api/portraits/women/30.jpg",
-    //     storyFile: "https://images.unsplash.com/photo-1520813792240-56fc4a3765a7"
-    // },
-    // {
-    //     fullName: "Benjamin Moore",
-    //     profilePic: "https://randomuser.me/api/portraits/men/77.jpg",
-    //     storyFile: "https://images.unsplash.com/photo-1492562080023-ab3db95bfbce"
-    // },
-    // {
-    //     fullName: "Isabella Clark",
-    //     profilePic: "https://randomuser.me/api/portraits/women/12.jpg",
-    //     storyFile: "https://images.unsplash.com/photo-1524504388940-b1c1722653e1"
-    // },
-    // {
-    //     fullName: "Lucas Rodriguez",
-    //     profilePic: "https://randomuser.me/api/portraits/men/51.jpg",
-    //     storyFile: "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d"
-    // },
-    // {
-    //     fullName: "Mia Lewis",
-    //     profilePic: "https://randomuser.me/api/portraits/women/47.jpg",
-    //     storyFile: "https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91"
-    // },
-    // {
-    //     fullName: "Henry Walker",
-    //     profilePic: "https://randomuser.me/api/portraits/men/9.jpg",
-    //     storyFile: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d"
-    // },
-    // {
-    //     fullName: "Charlotte Hall",
-    //     profilePic: "https://randomuser.me/api/portraits/women/55.jpg",
-    //     storyFile: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e"
-    // },
-    // {
-    //     fullName: "Daniel Young",
-    //     profilePic: "https://randomuser.me/api/portraits/men/63.jpg",
-    //     storyFile: "https://images.unsplash.com/photo-1519085360753-af0119f7cbe7"
-    // },
-    // {
-    //     fullName: "Amelia King",
-    //     profilePic: "https://randomuser.me/api/portraits/women/72.jpg",
-    //     storyFile: "https://images.unsplash.com/photo-1517841905240-472988babdf9"
-    // },
-    // {
-    //     fullName: "Matthew Scott",
-    //     profilePic: "https://randomuser.me/api/portraits/men/29.jpg",
-    //     storyFile: "https://images.unsplash.com/photo-1520813792240-56fc4a3765a7"
-    // },
-    // {
-    //     fullName: "Harper Green",
-    //     profilePic: "https://randomuser.me/api/portraits/women/81.jpg",
-    //     storyFile: "https://images.unsplash.com/photo-1494790108377-be9c29b29330"
-    // },
-    // {
-    //     fullName: "Ethan Baker",
-    //     profilePic: "https://randomuser.me/api/portraits/men/88.jpg",
-    //     storyFile: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e"
-    // },
-    // {
-    //     fullName: "Ella Adams",
-    //     profilePic: "https://randomuser.me/api/portraits/women/6.jpg",
-    //     storyFile: "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d"
-    // },
-    // {
-    //     fullName: "Alexander Nelson",
-    //     profilePic: "https://randomuser.me/api/portraits/men/14.jpg",
-    //     storyFile: "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d"
-    // }
+    {
+        fullName: "Sophia Martinez",
+        profilePic: "https://randomuser.me/api/portraits/women/13.jpg",
+        storyFile: "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?w=400&q=60"
+    },
+    {
+        fullName: "Noah Williams",
+        profilePic: "https://randomuser.me/api/portraits/men/14.jpg",
+        storyFile: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?w=400&q=60"
+    },
+    {
+        fullName: "Olivia Brown",
+        profilePic: "https://randomuser.me/api/portraits/women/15.jpg",
+        storyFile: "https://images.unsplash.com/photo-1470770903676-69b98201ea1c?w=400&q=60"
+    },
+    {
+        fullName: "James Anderson",
+        profilePic: "https://randomuser.me/api/portraits/men/16.jpg",
+        storyFile: "https://images.unsplash.com/photo-1493558103817-58b2924bce98?w=400&q=60"
+    },
+    {
+        fullName: "Ava Taylor",
+        profilePic: "https://randomuser.me/api/portraits/women/17.jpg",
+        storyFile: "https://images.unsplash.com/photo-1501785888041-af3ef285b470?w=400&q=60"
+    },
+    {
+        fullName: "Benjamin Moore",
+        profilePic: "https://randomuser.me/api/portraits/men/18.jpg",
+        storyFile: "https://images.unsplash.com/photo-1500534314209-a25ddb2bd429?w=400&q=60"
+    },
+    {
+        fullName: "Isabella Clark",
+        profilePic: "https://randomuser.me/api/portraits/women/19.jpg",
+        storyFile: "https://images.unsplash.com/photo-1502082553048-f009c37129b9?w=400&q=60"
+    },
+    {
+        fullName: "Lucas Rodriguez",
+        profilePic: "https://randomuser.me/api/portraits/men/20.jpg",
+        storyFile: "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?w=400&q=60"
+    },
+    {
+        fullName: "Mia Lewis",
+        profilePic: "https://randomuser.me/api/portraits/women/21.jpg",
+        storyFile: "https://images.unsplash.com/photo-1469474968028-56623f02e42e?w=400&q=60"
+    },
+    {
+        fullName: "Henry Walker",
+        profilePic: "https://randomuser.me/api/portraits/men/22.jpg",
+        storyFile: "https://images.unsplash.com/photo-1472214103451-9374bd1c798e?w=400&q=60"
+    },
+    {
+        fullName: "Charlotte Hall",
+        profilePic: "https://randomuser.me/api/portraits/women/23.jpg",
+        storyFile: "https://images.unsplash.com/photo-1501785888041-af3ef285b470?2?w=400&q=60"
+    },
+    {
+        fullName: "Daniel Young",
+        profilePic: "https://randomuser.me/api/portraits/men/24.jpg",
+        storyFile: "https://images.unsplash.com/photo-1482192596544-9eb780fc7f66?w=400&q=60"
+    },
+    {
+        fullName: "Amelia King",
+        profilePic: "https://randomuser.me/api/portraits/women/25.jpg",
+        storyFile: "https://images.unsplash.com/photo-1500534314209-a25ddb2bd429?2?w=400&q=60"
+    },
+    {
+        fullName: "Matthew Scott",
+        profilePic: "https://randomuser.me/api/portraits/men/26.jpg",
+        storyFile: "https://images.unsplash.com/photo-1502082553048-f009c37129b9?2?w=400&q=60"
+    },
+    {
+        fullName: "Harper Green",
+        profilePic: "https://randomuser.me/api/portraits/women/27.jpg",
+        storyFile: "https://images.unsplash.com/photo-1499346030926-9a72daac6c63?w=400&q=60"
+    },
+    {
+        fullName: "Ethan Baker",
+        profilePic: "https://randomuser.me/api/portraits/men/28.jpg",
+        storyFile: "https://images.unsplash.com/photo-1443890923422-7819ed4101c0?w=400&q=60"
+    },
+    {
+        fullName: "Ella Adams",
+        profilePic: "https://randomuser.me/api/portraits/women/29.jpg",
+        storyFile: "https://images.unsplash.com/photo-1474511320723-9a56873867b5?w=400&q=60"
+    },
+    {
+        fullName: "Alexander Nelson",
+        profilePic: "https://randomuser.me/api/portraits/men/30.jpg",
+        storyFile: "https://images.unsplash.com/photo-1501785888041-af3ef285b470?3?w=400&q=60"
+    }
 ];
 
 const createStoryHTMLThroughMap = () => {
     const returnedArr = stories.map((story) => {
         return `
                 <div class="story">
-                    <img src="${story.storyFile}" alt="">
+                    <div class="story-post">
+                        <img src="${story.storyFile}" alt="" loading="lazy">
+                    </div>                    
                     <div class="profile-pic">
                         <img src="${story.profilePic}" alt="">
                     </div>
@@ -136,6 +158,6 @@ const createStoryHTMLThroughMap = () => {
                 </div>        `
     });
 
-    storyContainer.innerHTML = returnedArr.join("");
+    storyContainer.innerHTML += returnedArr.join("");
 };
 createStoryHTMLThroughMap();
